bf1d6748437d1de0f05cd2ecc679646c43799d2b
sidharth3000 <201952234@iiitvadodara.ac.in>
2021-10-01 21:36:23 +0530

    added footeraa

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ import styles from "./Footer.module.css";
 import { AiFillGitlab } from "react-icons/ai";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillMail } from "react-icons/ai";
-import { AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => (
   <div className={styles.footer}>
