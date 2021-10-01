fc1e8cf397e4cd2bbc50bc6653783a8935f8ce45
sidharth3000 <201952234@iiitvadodara.ac.in>
2021-10-01 21:40:22 +0530

    contract redeployed

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import campaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(campaignFactory.interface),
-  "0x3D241488E6D961014e461E79e2fEbF2f5387f726"
+  "0x3D241488E6D961014e461E79e2fEbF2f5387f"
 );
 
 export default instance;
