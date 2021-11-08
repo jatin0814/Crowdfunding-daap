34b7125664d4f06dca2b32530de2c5c67a5973e3
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-11-09 01:41:22 +0530

    update

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
-      "/campaigns/new": { page: "/campaigns/new" },
-      "/campaigns/show": { page: "/campaigns/show" },
-      "/campaigns/requests/": { page: "/campaigns/requests/" },
-      "/campaigns/requests/new": { page: "/campaigns/requests/new" },
     };
   },
 };
